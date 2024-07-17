96623e1bae7e9e52e1308f06d67a2bff2dba77aa
alZyad <zyad.alalgui@theodo.com>
2024-07-17 15:02:55 +0200

    add pagination

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -2,31 +2,63 @@ import { useEffect, useState } from "react";
 import { StyleSheet, View, TextInput } from "react-native";
 import JokeList from "../JokeList/JokeList";
 import EmptyList from "../EmptyList/EmptyList";
-import { JokeType } from "../../types/joke.types";
+import { DataType, JokeType } from "../../types/joke.types";
 
-const getJokes = async (searchTerm: string) => {
+const getJokes = async (searchTerm?: string) => {
   try {
-    const response = await fetch(`https://v2.jokeapi.dev/joke/Programming?contains=${searchTerm}&amount=10`);
-    const json = await response.json();
-    return json;
+    const response1 = await fetch(`https://v2.jokeapi.dev/joke/Programming?contains=${searchTerm}&amount=10`);
+    const response2 = await fetch(`https://v2.jokeapi.dev/joke/Programming?contains=${searchTerm}&amount=10`);
+    const json1: DataType = await response1.json();
+    const json2: DataType = await response2.json();
+
+    return [json1, json2];
   } catch (error) {
-    console.error(">", error);
+    console.error(error);
   }
+};
+
+const mergeReceivedData = (data1?: DataType, data2?: DataType) => {
+  if (!data1 || !data2) return { error: true, amount: 0, jokes: [] };
+  if (!data1.jokes) return data2;
+  if (!data2.jokes) return data1;
+
+  let mergedData = data1;
+  const jokes1: JokeType[] = data1.jokes;
+  const jokes2: JokeType[] = data2.jokes;
+  const mergedJokes = jokes1.reduce<JokeType[]>((acc, joke) => {
+    if (acc.every((existingJoke) => existingJoke.id !== joke.id)) return [...acc, joke];
+    return acc;
+  }, jokes2);
+  mergedData.jokes = mergedJokes;
+  return mergedData;
 };
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [data, setData] = useState<{ error: boolean; jokes: JokeType[]; message: string }>();
+  const [loadJokes, setLoadJokes] = useState(0);
+  const [data, setData] = useState<DataType>();
+  useEffect(() => {
+    getJokes(searchTerm).then((receivedData) => {
+      if (!receivedData) return;
+      setData(mergeReceivedData(receivedData[0], receivedData[1]));
+    });
+  }, [searchTerm]);
 
   useEffect(() => {
-    getJokes(searchTerm).then((receivedData) => setData(receivedData));
-  }, [searchTerm]);
+    if (!loadJokes) return;
+    getJokes(searchTerm).then((receivedData) => {
+      if (!receivedData) return;
+      const mergedRequests = mergeReceivedData(receivedData[0], receivedData[1]);
+      console.log("going to set data");
+      setData((prevValue) => mergeReceivedData(mergedRequests, prevValue));
+    });
+  }, [loadJokes, searchTerm]);
 
   if (!data) return <EmptyList />;
   return (
     <View style={styles.container}>
       <TextInput style={styles.input} value={searchTerm} onChangeText={setSearchTerm} testID="searchInput" />
-      <JokeList {...data} />
+      <JokeList {...data} setLoadJokes={setLoadJokes} />
     </View>
   );
 }
@@ -34,10 +66,10 @@ export default function Home() {
 const styles = StyleSheet.create({
   input: {
     height: 40,
-    margin: 12,
     borderWidth: 1,
-    padding: 10,
     width: 300,
+    margin: 10,
+    padding: 10,
   },
   container: {
     paddingTop: 30,
--- components/JokeList/JokeList.tsx
+++ components/JokeList/JokeList.tsx
@@ -1,19 +1,47 @@
 import { JokeType } from "../../types/joke.types";
 import Error from "../Error/Error";
 import JokePreview from "../JokePreview/JokePreview";
-import { StyleSheet, ScrollView } from "react-native";
+import { StyleSheet, ScrollView, View, NativeScrollEvent } from "react-native";
 import EmptyList from "../EmptyList/EmptyList";
+import { Dispatch, SetStateAction, useState } from "react";
 
-export default function JokeList({ error, jokes, message }: { error: boolean; jokes: JokeType[]; message: string }) {
+const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }: NativeScrollEvent) => {
+  const paddingToBottom = 20;
+  return layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom;
+};
+
+export default function JokeList({
+  error,
+  jokes,
+  message,
+  setLoadJokes,
+}: {
+  error: boolean;
+  jokes: JokeType[];
+  message?: string;
+  setLoadJokes: Dispatch<SetStateAction<number>>;
+}) {
+  const [atBottom, setAtBottom] = useState(false);
+
   if (error && message === "No matching joke found") {
     return <EmptyList />;
   }
   if (error && message !== "No matching joke found") return <Error />;
   return (
-    <ScrollView contentContainerStyle={styles.listContent} style={styles.scrollList}>
+    <ScrollView
+      contentContainerStyle={styles.listContent}
+      style={styles.scrollList}
+      onScroll={({ nativeEvent }) => {
+        if (!atBottom && isCloseToBottom(nativeEvent)) {
+          setLoadJokes((prevValue) => prevValue + 1);
+        }
+        setAtBottom(isCloseToBottom(nativeEvent));
+      }}
+    >
       {jokes.map((jokeData) => (
         <JokePreview {...jokeData} key={jokeData.id} />
       ))}
+      {jokes.length % 2 === 1 && <View style={styles.emptyPreview} />}
     </ScrollView>
   );
 }
@@ -25,8 +53,15 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     justifyContent: "center",
     gap: 25,
+    paddingTop: 20,
+    paddingBottom: 50,
   },
   scrollList: {
     width: "100%",
+  },
+  emptyPreview: {
+    padding: 5,
+    width: 150,
+    height: 50,
   },
 });
